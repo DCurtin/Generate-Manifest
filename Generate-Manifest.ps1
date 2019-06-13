73ad89c97bd731057089d4dd90a1a7e24c41a31f
Dalton Curtin <dcurtin@midland.local>
2019-06-13 09:23:48 -0400

    Mostly working code

--- Generate-Manifest.ps1
+++ Generate-Manifest.ps1
@@ -1,47 +1,58 @@
-﻿function addElementsToManifest($name, $member, $manifest)
+﻿function addElementsToManifest
 {
-
-    #echo "$name, $member, $manifest"
+    param(
+    [string]$name='',
+    [string]$member='',
+    [XML]$manifest=$null
+    )
     
     if($manifest -eq $null)
     {
         return
     }
 
-
     $types_element = $manifest.CreateElement('types', "http://soap.sforce.com/2006/04/metadata")
 
     $types_members_element = $manifest.CreateElement('members', 'http://soap.sforce.com/2006/04/metadata')
-    $types_members_element.InnerText=$member > $null
+    $types_members_element.InnerText=$member
     $types_element.AppendChild($types_members_element)
     
     $types_name_element = $manifest.CreateElement('name', 'http://soap.sforce.com/2006/04/metadata')
-    $types_name_element.InnerText=$name > $null
+    $types_name_element.InnerText=$name
     $types_element.AppendChild($types_name_element)
     
     $manifest.Package.AppendChild($types_element)
+
+    return $manifest
 }
 function Generate-Manifest
 {
     param(
     [string]$regexClass='',
     [string]$regexTrigger='',
-    [string]$path='.'
+    [string]$path=$(pwd)
     )
-
-    New-Item -Path "$path\+package.xml" -Force > $null
 
     if($regexClass -eq '' -and $regexTrigger -eq '')
     {
-        echo 'Please provide at least one argument: Generate-Manfiest -regexClass "\w*test$" or Generate-Manfiest -regexTrigger "\w*triggerendswiththis$"' 
+        echo "`nPlease provide at least one argument `n`n e.g.`tGenerate-Manifest -regexClass '^Trail\w*test$'`t [begins with Trail ends with test]
+        Generate-Manifest -regexTrigger 'testString$'`t [begins with anything, ends with testString]
+        Generate-Manifest -regexClass 'test,merge'`t`t [any class with test or merge in it's name]"
         return
     }
+
+    New-Item -Path "$path\package.xml" -Force > $null
+
+    [System.Collections.ArrayList] $regexClassList = $regexClass.Split(',')
+    [System.Collections.ArrayList] $regexTriggerList = $regexTrigger.Split(',')
+
+
     echo 'querying class names'
 
     [System.Collections.ArrayList] $classNames = sfdx force:data:soql:query -q "SELECT Name FROM ApexClass" -r csv -u 'dcurtin@midlandira.com'
-    $classNames.RemoveAt(0)
+    $classNames.RemoveAt(0)#remove header
     [System.Collections.ArrayList] $triggerNames = sfdx force:data:soql:query -q "SELECT Name FROM ApexTrigger" -r csv -u 'dcurtin@midlandira.com'
-    $triggerNames.RemoveAt(0)
+    $triggerNames.RemoveAt(0)#remove header
     
     [XML]$BLANK_MAN='<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
     <Package xmlns="http://soap.sforce.com/2006/04/metadata">
@@ -50,30 +61,34 @@ function Generate-Manifest
     
     echo 'Starting Package Creation'
 
-    if($regexClass -eq '')
+    if($regexClassList[0] -ne '')
     {
-        $classNames.ForEach(
+        foreach($class in $classNames)
+        {
+            foreach($regex in $regexClassList)
             {
-                if($_ -match $regexClass)
+                if($class -match $regex)
                 { 
                     #echo "ApexClass, $_, $BLANK_MAN"
-                    addElementsToManifest 'ApexClass' $_ $BLANK_MAN
+                    addElementsToManifest 'ApexClass' $class $BLANK_MAN
                 }
             }
-        )
+        }
     }
 
-    if($regexTrigger -eq '')
+    if($regexTriggerList[0] -ne '')
     {
-        $triggerNames.ForEach(
+        foreach($trigger in $triggerNames)
+        {
+            foreach($regex in $regexTriggerList)
             {
-                if($_ -match $regexTrigger)
-                {
-                    #echo "ApexTrigger, $_, $BLANK_MAN"
-                    addElementsToManifest 'ApexTrigger' $_ $BLANK_MAN
+                if($trigger -match $regex)
+                { 
+                    #echo "ApexClass, $_, $BLANK_MAN"
+                    addElementsToManifest 'ApexTrigger' $trigger $BLANK_MAN
                 }
             }
-        )
+        }
     }
     
     echo 'generating package.xml'
